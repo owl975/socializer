bdf751e603fb130d2842a0db68bd5557c8237e9d
mtgomez <owl975@gmail.com>
2015-10-05 16:48:14 -0700

    custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users
+  devise_scope :user do
+  	get '/register', to: 'devise/registrations#new', as: :register
+  	get '/login', to: 'devise/sessions#new', as: :login
+  	get '/logout', to: 'devise/sessions#destroy', as: :logout
+  end
   resources :statuses
 
   root to: 'statuses#index'
+
 
 
 end
