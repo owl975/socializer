d83236cd8bdd723b5a7f867fb688d5f0cf9d0d8d
mtgomez <owl975@gmail.com>
2015-10-07 15:12:42 -0700

    generated profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :views
   devise_for :users
   devise_scope :user do
@@ -10,12 +12,25 @@ Rails.application.routes.draw do
 
   root to: 'statuses#index'
 
-
-
 end
 
 
-#   Prefix Verb   URI Pattern                    Controller#Action
+#  Prefix Verb   URI Pattern                    Controller#Action
+#         new_view_session GET    /views/sign_in(.:format)       devise/sessions#new
+#             view_session POST   /views/sign_in(.:format)       devise/sessions#create
+#     destroy_view_session DELETE /views/sign_out(.:format)      devise/sessions#destroy
+#            view_password POST   /views/password(.:format)      devise/passwords#create
+#        new_view_password GET    /views/password/new(.:format)  devise/passwords#new
+#       edit_view_password GET    /views/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /views/password(.:format)      devise/passwords#update
+#                          PUT    /views/password(.:format)      devise/passwords#update
+# cancel_view_registration GET    /views/cancel(.:format)        devise/registrations#cancel
+#        view_registration POST   /views(.:format)               devise/registrations#create
+#    new_view_registration GET    /views/sign_up(.:format)       devise/registrations#new
+#   edit_view_registration GET    /views/edit(.:format)          devise/registrations#edit
+#                          PATCH  /views(.:format)               devise/registrations#update
+#                          PUT    /views(.:format)               devise/registrations#update
+#                          DELETE /views(.:format)               devise/registrations#destroy
 #         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
 #             user_session POST   /users/sign_in(.:format)       devise/sessions#create
 #     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
@@ -31,6 +46,9 @@ end
 #                          PATCH  /users(.:format)               devise/registrations#update
 #                          PUT    /users(.:format)               devise/registrations#update
 #                          DELETE /users(.:format)               devise/registrations#destroy
+#                 register GET    /register(.:format)            devise/registrations#new
+#                    login GET    /login(.:format)               devise/sessions#new
+#                   logout GET    /logout(.:format)              devise/sessions#destroy
 #                 statuses GET    /statuses(.:format)            statuses#index
 #                          POST   /statuses(.:format)            statuses#create
 #               new_status GET    /statuses/new(.:format)        statuses#new
